bae6137f457227e176d3336a410e6ecff9cec16e
Douglas Kirby <douglas.r.kirby@gmail.com>
2026-04-15 02:02:05 -0400

    commented out likely unneeded code

--- processing/engine.go
+++ processing/engine.go
@@ -3,7 +3,6 @@ package processing
 import (
 	"fmt"
 	"net"
-	"sort"
 	"sync"
 	"time"
 )
@@ -30,14 +29,14 @@ const (
 )
 
 type Engine struct {
-	L                    *lua.LState
-	m                    sync.Mutex
-	values               map[string]emulator.Value
+	// L                    *lua.LState
+	m sync.Mutex
+	// values               map[string]emulator.Value
 	conn                 net.PacketConn
 	osAddr               *net.UDPAddr
 	openSplitConnected   bool
 	opensplitConnectedCh chan bool
-	tickFunc             *lua.LFunction
+	// tickFunc             *lua.LFunction
 }
 
 func NewEngine() (*Engine, chan bool) {
@@ -52,8 +51,8 @@ func NewEngine() (*Engine, chan bool) {
 	}
 
 	e := &Engine{
-		m:                    sync.Mutex{},
-		values:               make(map[string]emulator.Value),
+		m: sync.Mutex{},
+		// values:               make(map[string]emulator.Value),
 		conn:                 conn,
 		osAddr:               addr,
 		opensplitConnectedCh: make(chan bool),
@@ -73,9 +72,9 @@ func NewEngine() (*Engine, chan bool) {
 }
 
 func (e *Engine) Close() {
-	if e.L != nil {
-		e.L.Close()
-	}
+	// if e.L != nil {
+	// e.L.Close()
+	// }
 	e.updateConnectionStatus(false)
 	_ = e.conn.Close()
 	e.conn = nil
@@ -85,140 +84,140 @@ func (e *Engine) OpenSplitConnected() bool {
 	return e.openSplitConnected
 }
 
-func (e *Engine) LoadFile(path string, plan *emulator.ReadPlan) error {
-	L := lua.NewState()
-	e.L = L
+// func (e *Engine) LoadFile(path string/*, plan *emulator.ReadPlan*/) error {
+// 	L := lua.NewState()
+// 	e.L = L
 
-	for _, spec := range plan.Watches {
-		if spec.Type == emulator.Bool {
-			e.L.SetGlobal(spec.Name, lua.LBool(false))
-			e.L.SetGlobal(spec.Name+"_last", lua.LBool(false))
-		} else {
-			e.L.SetGlobal(spec.Name, lua.LNumber(0))
-			e.L.SetGlobal(spec.Name+"_last", lua.LNumber(0))
-		}
-	}
+// 	for _, spec := range plan.Watches {
+// 		if spec.Type == emulator.Bool {
+// 			e.L.SetGlobal(spec.Name, lua.LBool(false))
+// 			e.L.SetGlobal(spec.Name+"_last", lua.LBool(false))
+// 		} else {
+// 			e.L.SetGlobal(spec.Name, lua.LNumber(0))
+// 			e.L.SetGlobal(spec.Name+"_last", lua.LNumber(0))
+// 		}
+// 	}
 
-	e.L.SetGlobal("split", e.L.NewFunction(func(L *lua.LState) int {
-		packet := buildRCPacket(SPLIT, false)
+// 	e.L.SetGlobal("split", e.L.NewFunction(func(L *lua.LState) int {
+// 		packet := buildRCPacket(SPLIT, false)
 
-		e.m.Lock()
-		defer e.m.Unlock()
+// 		e.m.Lock()
+// 		defer e.m.Unlock()
 
-		_, err := e.conn.WriteTo(packet, e.osAddr)
-		if err != nil {
-			fmt.Println(err)
-			e.updateConnectionStatus(false)
-			return 1
-		}
-		e.updateConnectionStatus(true)
-		return 0
-	}))
+// 		_, err := e.conn.WriteTo(packet, e.osAddr)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			e.updateConnectionStatus(false)
+// 			return 1
+// 		}
+// 		e.updateConnectionStatus(true)
+// 		return 0
+// 	}))
 
-	e.L.SetGlobal("reset", e.L.NewFunction(func(L *lua.LState) int {
-		packet := buildRCPacket(RESET, false)
+// 	e.L.SetGlobal("reset", e.L.NewFunction(func(L *lua.LState) int {
+// 		packet := buildRCPacket(RESET, false)
 
-		e.m.Lock()
-		defer e.m.Unlock()
+// 		e.m.Lock()
+// 		defer e.m.Unlock()
 
-		_, err := e.conn.WriteTo(packet, e.osAddr)
-		if err != nil {
-			e.updateConnectionStatus(false)
-			fmt.Println(err)
-			return 1
-		}
-		e.updateConnectionStatus(true)
-		return 0
-	}))
+// 		_, err := e.conn.WriteTo(packet, e.osAddr)
+// 		if err != nil {
+// 			e.updateConnectionStatus(false)
+// 			fmt.Println(err)
+// 			return 1
+// 		}
+// 		e.updateConnectionStatus(true)
+// 		return 0
+// 	}))
 
-	e.L.SetGlobal("pause", e.L.NewFunction(func(L *lua.LState) int {
-		packet := buildRCPacket(PAUSE, false)
+// 	e.L.SetGlobal("pause", e.L.NewFunction(func(L *lua.LState) int {
+// 		packet := buildRCPacket(PAUSE, false)
 
-		e.m.Lock()
-		defer e.m.Unlock()
+// 		e.m.Lock()
+// 		defer e.m.Unlock()
 
-		_, err := e.conn.WriteTo(packet, e.osAddr)
-		if err != nil {
-			e.updateConnectionStatus(false)
-			fmt.Println(err)
-			return 1
-		}
-		e.updateConnectionStatus(true)
-		return 0
-	}))
+// 		_, err := e.conn.WriteTo(packet, e.osAddr)
+// 		if err != nil {
+// 			e.updateConnectionStatus(false)
+// 			fmt.Println(err)
+// 			return 1
+// 		}
+// 		e.updateConnectionStatus(true)
+// 		return 0
+// 	}))
 
-	if err := e.L.DoFile(path); err != nil {
-		return err
-	}
+// 	if err := e.L.DoFile(path); err != nil {
+// 		return err
+// 	}
 
-	fn := e.L.GetGlobal("onTick")
-	if fn.Type() != lua.LTFunction {
-		fmt.Println("onTick not present in factfinder.lua")
-	} else {
-		e.tickFunc = fn.(*lua.LFunction)
-	}
+// 	fn := e.L.GetGlobal("onTick")
+// 	if fn.Type() != lua.LTFunction {
+// 		fmt.Println("onTick not present in factfinder.lua")
+// 	} else {
+// 		e.tickFunc = fn.(*lua.LFunction)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func (e *Engine) GetState() [][]string {
-	out := make([][]string, 0)
+// func (e *Engine) GetState() [][]string {
+// 	out := make([][]string, 0)
 
-	tbl, ok := e.L.GetGlobal("state").(*lua.LTable)
-	if !ok {
-		return out
-	}
+// 	tbl, ok := e.L.GetGlobal("state").(*lua.LTable)
+// 	if !ok {
+// 		return out
+// 	}
 
-	tbl.ForEach(func(k, v lua.LValue) {
-		out = append(out, []string{k.String(), v.String()})
-	})
+// 	tbl.ForEach(func(k, v lua.LValue) {
+// 		out = append(out, []string{k.String(), v.String()})
+// 	})
 
-	sort.Slice(out, func(i, j int) bool {
-		return out[i][0] < out[j][0] // sort by key
-	})
+// 	sort.Slice(out, func(i, j int) bool {
+// 		return out[i][0] < out[j][0] // sort by key
+// 	})
 
-	return out
-}
+// 	return out
+// }
 
-func (e *Engine) ProcessValues(values []emulator.Value) error {
-	for _, newValue := range values {
-		name := newValue.Name
-		t := newValue.Type
+// func (e *Engine) ProcessValues(values []emulator.Value) error {
+// 	for _, newValue := range values {
+// 		name := newValue.Name
+// 		t := newValue.Type
 
-		// First time we've seen this value, do no processing on it yet.
-		if _, ok := e.values[name]; !ok {
-			e.values[name] = newValue
-			continue
-		}
+// 		// First time we've seen this value, do no processing on it yet.
+// 		if _, ok := e.values[name]; !ok {
+// 			e.values[name] = newValue
+// 			continue
+// 		}
 
-		switch t {
-		case emulator.FlagCount:
-			e.L.SetGlobal(name+"_last", lua.LNumber(e.values[name].FlagCount))
-			e.L.SetGlobal(name, lua.LNumber(newValue.FlagCount))
-		case emulator.Bool:
-			e.L.SetGlobal(name+"_last", lua.LBool(e.values[name].Bool))
-			e.L.SetGlobal(name, lua.LBool(newValue.Bool))
-		case emulator.U8, emulator.U16, emulator.U32, emulator.U64:
-			e.L.SetGlobal(name+"_last", lua.LNumber(e.values[name].Unsigned))
-			e.L.SetGlobal(name, lua.LNumber(newValue.Unsigned))
-		default:
-			e.L.SetGlobal(name+"_last", lua.LNumber(e.values[name].Signed))
-			e.L.SetGlobal(name, lua.LNumber(newValue.Signed))
-		}
+// 		switch t {
+// 		case emulator.FlagCount:
+// 			e.L.SetGlobal(name+"_last", lua.LNumber(e.values[name].FlagCount))
+// 			e.L.SetGlobal(name, lua.LNumber(newValue.FlagCount))
+// 		case emulator.Bool:
+// 			e.L.SetGlobal(name+"_last", lua.LBool(e.values[name].Bool))
+// 			e.L.SetGlobal(name, lua.LBool(newValue.Bool))
+// 		case emulator.U8, emulator.U16, emulator.U32, emulator.U64:
+// 			e.L.SetGlobal(name+"_last", lua.LNumber(e.values[name].Unsigned))
+// 			e.L.SetGlobal(name, lua.LNumber(newValue.Unsigned))
+// 		default:
+// 			e.L.SetGlobal(name+"_last", lua.LNumber(e.values[name].Signed))
+// 			e.L.SetGlobal(name, lua.LNumber(newValue.Signed))
+// 		}
 
-		e.values[name] = newValue
-	}
+// 		e.values[name] = newValue
+// 	}
 
-	err := e.L.CallByParam(lua.P{
-		Fn:      e.tickFunc,
-		NRet:    0,
-		Protect: true,
-	})
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// 	err := e.L.CallByParam(lua.P{
+// 		Fn:      e.tickFunc,
+// 		NRet:    0,
+// 		Protect: true,
+// 	})
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
 func (e *Engine) Hello() bool {
 	packet := buildRCPacket(HELLO, true)
