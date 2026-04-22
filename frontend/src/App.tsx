07129f24e8140a1b0f17e032bbe74f7546387f98
Douglas Kirby <douglas.r.kirby@gmail.com>
2026-04-21 21:42:30 -0400

    removed demo frontend; added Oauth button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,31 @@
 import {useState} from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import * as racetime from "../wailsjs/go/main/App";
+import { LoginWithOAuth, RaceListWindow } from './components/racetime_gg';
+import { WindowSetSize } from "../wailsjs/runtime";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
-
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
-
+    WindowSetSize(320, 580);
     return (
         <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
+            <button
+                onClick={async () => {
+                    await LoginWithOAuth();
+                }}
+            >
+                Racetime.gg Auth
+            </button>
+
+            {/* <button
+                hidden
+                onClick={async () => {
+                    // TODO: Get this shit to work
+                    await RaceListWindow();
+                }}
+            >
+                Racetime.gg Races
+            </button> */}
         </div>
     )
 }
