92b56a49ae19bc44ea1a8c6dd3d3364cd495d294
Douglas Kirby <douglas.r.kirby@gmail.com>
2026-04-23 19:11:36 -0400

    implemented state and effect tracking to manage frontend display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,50 @@
-import {useState} from 'react';
+import { useEffect, useState } from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
 import * as racetime from "../wailsjs/go/main/App";
-import { LoginWithOAuth, RaceListWindow } from './components/racetime_gg';
+import { LoginWithOAuth, RaceList, RaceListWindow } from './components/racetime_gg';
 import { WindowSetSize } from "../wailsjs/runtime";
 
 function App() {
-    WindowSetSize(320, 580);
-    return (
-        <div id="App">
-            <button
-                onClick={async () => {
-                    await LoginWithOAuth();
-                }}
-            >
-                Racetime.gg Auth
-            </button>
+    const [token, setToken] = useState("")
+    const [raceList, setRaceList] = useState([])
+    useEffect(() => {
+        // call backend function to get token
+        // setToken to return
+        // if (await CheckTokens()) {
+        // return
+        // }
+        async () => {
+                const raceToken = await racetime.GetAccessToken()
+                setToken(raceToken)
+            }
+    }, [])
+    useEffect(() => {
+        if (token == "") {
+            return
+        }
 
-            {/* <button
+        (
+            async () => {
+                const raceObj = await RaceList()
+                setRaceList(raceObj["races"])
+            }
+        )()
+    }, [token])
+
+    WindowSetSize(320, 580);
+    if (token == "") {
+        return (
+            <div id="App">
+                <button
+                    onClick={async () => {
+                        await LoginWithOAuth();
+                    }}
+                >
+                    Racetime.gg Auth
+                </button>
+
+                {/* <button
                 hidden
                 onClick={async () => {
                     // TODO: Get this shit to work
@@ -26,6 +53,16 @@ function App() {
             >
                 Racetime.gg Races
             </button> */}
+            </div>
+        )
+    }
+    return (
+        <div id="App">
+            <ul>
+            {
+                raceList.map((r)=><li>{r["name"]}</li>)
+            }
+            </ul>
         </div>
     )
 }
