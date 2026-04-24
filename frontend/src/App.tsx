4f72ed6b071edd92e0779f1504ed4ffd8f08097b
Douglas Kirby <douglas.r.kirby@gmail.com>
2026-04-24 02:52:53 -0400

    changed to CheckTokens added comments added initial display code for race

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,24 @@ import { useEffect, useState } from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
 import * as racetime from "../wailsjs/go/main/App";
-import { LoginWithOAuth, RaceList, RaceListWindow } from './components/racetime_gg';
+import { LoginWithOAuth, RaceList/*, RaceListWindow*/ } from './components/racetime_gg';
 import { WindowSetSize } from "../wailsjs/runtime";
 
 function App() {
     const [token, setToken] = useState("")
     const [raceList, setRaceList] = useState([])
+    const [race, setJoinedRace] = useState("")
+
+    // Gets tokens from backend
     useEffect(() => {
         // call backend function to get token
-        // setToken to return
-        // if (await CheckTokens()) {
-        // return
-        // }
         async () => {
-                const raceToken = await racetime.GetAccessToken()
-                setToken(raceToken)
-            }
+            const raceToken = await racetime.CheckTokens()
+            setToken(raceToken)
+        }
     }, [])
+
+    // Gets list of races
     useEffect(() => {
         if (token == "") {
             return
@@ -32,8 +33,24 @@ function App() {
         )()
     }, [token])
 
+    // Gets selected race
+    // useEffect(() => {
+    //     if (race == "") {
+    //         return
+    //     }
+
+    //     (
+    //         async () => {
+    //             const selectedRace = await GetRace()
+    //             setJoinedRace(selectedRace)
+    //         }
+    //     )()
+    // }, [])
+
     WindowSetSize(320, 580);
     if (token == "") {
+        // no token
+        // show login button
         return (
             <div id="App">
                 <button
@@ -45,16 +62,33 @@ function App() {
                 </button>
             </div>
         )
+    } else {
+        if (race == "") {
+            // no race
+            // show race list buttons
+            return (
+                <div id="App">
+                    <ul>
+                        {
+                            raceList.map((r) => <li>{r["name"]}</li>)
+                        }
+                    </ul>
+                </div>
+            )
+        } else {
+            // race selected
+            // show race window
+            return (
+                <div id="App">
+                    {/* <ul>
+                        {
+                            raceList.map((r) => <li>{r["name"]}</li>)
+                        }
+                    </ul> */}
+                </div>
+            )
+        }
     }
-    return (
-        <div id="App">
-            <ul>
-            {
-                raceList.map((r)=><li>{r["name"]}</li>)
-            }
-            </ul>
-        </div>
-    )
 }
 
 export default App
